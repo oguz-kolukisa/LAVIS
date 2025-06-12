8769c12a1570f818612695117e65fe35d7b98b7d
oguz-kolukisa <oguzkolukisa1@gmail.com>
2025-06-12 13:29:25 +0300

    Add VQA v2 fine-tuning configs for LLaMA 3.1

--- lavis/models/__init__.py
+++ lavis/models/__init__.py
@@ -33,6 +33,7 @@ from lavis.models.blip_models.blip_vqa import BlipVQA
 
 from lavis.models.blip2_models.blip2 import Blip2Base
 from lavis.models.blip2_models.blip2_opt import Blip2OPT
+from lavis.models.blip2_models.blip2_llama import Blip2Llama
 from lavis.models.blip2_models.blip2_t5 import Blip2T5
 from lavis.models.blip2_models.blip2_qformer import Blip2Qformer
 from lavis.models.blip2_models.blip2_image_text_matching import Blip2ITM
@@ -80,6 +81,7 @@ __all__ = [
     "Blip2Base",
     "Blip2ITM",
     "Blip2OPT",
+    "Blip2Llama",
     "Blip2T5",
     "Blip2T5Instruct",
     "Blip2VicunaInstruct",
